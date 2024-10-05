38d9847e5cf70b80b5b3a0bbffb9105a49bae061
tonygeorge13 <tonygeorge1312004@gmail.com>
2024-10-05 04:25:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.getElementById('img');
+let container = document.querySelector(".container")
+function phones(phone) {
+    img.src = phone ; 
+}
+function colors(color) {
+    container.style.background = color;
+}
